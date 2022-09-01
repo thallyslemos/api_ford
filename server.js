cb7562a7d35007ffd4cb50a496187ac12e95a950
Thallys <thallysvilemos@gmail.com>
2022-08-31 23:12:59 -0300

    Ultima de hj :´(

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const http = require("http");
+// const express = require('express');
 const app = require("./config/express");
 
 const port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.send('Opaaaa')
+});
+
+app.listen(port, () => {
+  console.log('Aplicação rodando em: ' + port)
+})
+
 
 http.createServer(app).listen(this.port, function () {
   console.log("Servidor escutando na porta: " + this.address().port);
