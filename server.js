b9fb2b232b71bfdc5aae462a67c8ce59edde08af
Thallys <thallysvilemos@gmail.com>
2022-08-31 22:27:03 -0300

    Primeira modificação

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-const http = require("http");
-const app = require("./config/express");
+const express = require('express');
+const app = express();
 
-http.createServer(app).listen(3000, function () {
-  console.log("Servidor escutando na porta: " + this.address().port);
-});
+const port = process.env.PORT || 3000;
+
+app.get("/", function (rew, res) {
+  res.send("Olá")
+})
+
+app.listen(port, () => {
+  console.info("Servidor escutando na porta: sei lá" )
+})
+// const http = require("http");
+// const app = require("./config/express");
+
+// http.createServer(app).listen(3000, function () {
+//   console.log("Servidor escutando na porta: " + this.address().port);
+// });
